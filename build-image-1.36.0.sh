5e3f8e7a963ca09c837d44f9ab70806d512c2d74
Walter Medvedeo <wmedvedeo@gmail.com>
2025-08-08 18:03:02 +0200

    Add comments to facilitate the build with OSL 1.35.0

--- build-image-1.36.0.sh
+++ build-image-1.36.0.sh
@@ -5,6 +5,7 @@ REGISTRY=quay.io
 
 IMG=$REGISTRY/$USER/serverless-workflow-operator-subflows:1.0
 
+# Given the structuring of current project, the docker build is started using the current src/main/resources as context.
 docker build -f ./1.36.0/docker/Dockerfile \
   --tag $IMG \
   ./src/main/resources
